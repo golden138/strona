fee90d2b4cd88717bfaae637af7db9751bcab045
golden138 <golden.style.91@gmail.com>
2025-04-13 14:15:51 +0200

    Update zadania.js

--- zadania.js
+++ zadania.js
@@ -20,3 +20,30 @@ function sprawdz(x) {
       document.getElementById(gdzie).innerText = 'Nie udało się pobrać rozwiązania.';
     });
 }
+
+function zaladuj(x) {
+	var tresc = '/strona/chemia/zadania/'+x+'/zad.txt';
+	var nr = '';
+	for(var i = 3; i < x.length; i++) {
+		nr += x.charAt(i);
+	}
+	fetch(tresc) // <-- tutaj wpisz adres pliku z zadaniem
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Błąd sieci!');
+      }
+      return response.text();
+    })
+    .then(data => {
+		var zdj;
+		var tresc_zad = data;
+		var dane = '<div class="zad"><img src="img/ph-meter.png" />Zadanie ' + nr + '.</div><p>' + data + '</p><div class="roz" onclick="sprawdz(\'zad'+ nr +'\');">Rozwiązani</div><div id="roz' + nr + '"></div>';';
+		document.getElementById('zad1').innerHTML = dane;
+    })
+    .catch(error => {
+		console.error('Wystąpił błąd:', error);
+		document.getElementById(gdzie).innerHTML = '<div class="zad">Zadanie ' + nr + '.</div><p>Nie udało się pobrać treści zadania</p>';
+    });
+}
+
+window.onload = zaladuj('zad1');
