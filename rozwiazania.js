3684e314a6e576cb607b6316d2e823d14ce2a24c
golden138 <golden.style.91@gmail.com>
2025-04-13 09:47:08 +0200

    Add files via upload
    pierwsze uruchomienie

---
+++ rozwiazania.js
@@ -0,0 +1,17 @@
+
+function sprawdz() {
+	fetch('file:///C:/Users/sebas/Desktop/Golden/Strona/chemia/zadania/zad1/roz.txt') // <-- tutaj wpisz adres pliku z zadaniem
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Błąd sieci!');
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('roz1').innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Wystąpił błąd:', error);
+      document.getElementById('roz1').innerText = 'Nie udało się pobrać rozwiązania.';
+    });
+}
