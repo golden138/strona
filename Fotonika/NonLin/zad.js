92fd37fd91757881363f2c50b20b592d1f1c4c3e
golden138 <golden.style.91@gmail.com>
2025-11-20 11:36:44 +0100

    update

---
+++ Fotonika/NonLin/zad.js
@@ -0,0 +1,37 @@
+var path = '/strona/Fotonika/NonLin';
+
+function start() {
+	setPath(path);
+	getArtykul();
+	// Wczytaj wartość przy otwarciu
+	//localStorage.setItem("status", 'Post1');
+    const statusPath = localStorage.getItem("status");
+	var gdzie = path + '/Posty/' + statusPath + '/nr_zadan.txt';
+	var tresc = '';
+	fetch(gdzie) // <-- tutaj wpisz adres pliku z zadaniem
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Błąd sieci!');
+      }
+      return response.text();
+    })
+    .then(data => {
+		var tablica = data.trim().split(/\s+/);
+		for(var i = 0; i < tablica.length; i++) {
+			console.log(tablica[i]);
+			tresc += '<div id="'+tablica[i]+'"></div>';
+		}
+		document.getElementById("Wszystkie_zadania").innerHTML = tresc;
+		for(var i = 0; i < tablica.length; i++) {
+			zaladuj(tablica[i]);
+		}
+    })
+    .catch(error => {
+		console.error('Wystąpił błąd:', error);
+    });
+	setTimeout(function() {
+  	MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    }, 500);
+}
+
+window.onload = start();
